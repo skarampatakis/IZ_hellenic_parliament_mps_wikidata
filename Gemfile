6d5bc8e18a314a8408880901b1bee789b45d2298
skarampatakis <s.karampatakis@gmail.com>
2017-11-24 14:12:06 +0200

    updated scraper

--- Gemfile
+++ Gemfile
@@ -4,7 +4,13 @@
 
 source "https://rubygems.org"
 
-ruby "2.0.0"
+ruby "2.4.1"
 
 gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
 gem "mechanize"
+gem "dotenv"
+gem "rest-client"
+#gem 'open-uri-cached'
+#gem "rest-client-components", git: "https://github.com/amatriain/rest-client-components.git", branch: "rest-client-2-compatibility"
+#gem "rack-cache"
+gem "sparql"
--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,102 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
+ require 'scraperwiki'
+ #require 'mechanize'
+ require 'rest-client'
+ require 'json'
+ require "dotenv"
+ require 'sparql/client'
+ #require 'restclient/components'
+ #require 'rack/cache'
+ #RestClient.enable Rack::Cache
+ Dotenv.load
+
+def mpId(value)
+  begin
+    mpId = value["mpID"].value
+  rescue
+    mpId = nil
+  end
+  mpId
+end
+
+def birthDate(value)
+  begin
+    birthDate = value["birthDate"].value
+  rescue
+    birthDate = nil
+  end
+  birthDate
+end
+
+def birthPlace(value)
+  begin
+    birthPlace = value["birthPlace"].value
+  rescue
+    birthPlace = nil
+  end
+  birthPlace
+end
+
+def gender(value)
+  begin
+    gender = value["gender"].value
+  rescue
+    gender = nil
+  end
+  gender
+end
+
+def mpStatus(value)
+  begin
+    mpStatus = value["mpStatus"].value
+  rescue
+    mpStatus = nil
+  end
+  mpStatus
+end
+
+def getWikidata(id, original)
+  begin
+    endpoint = "https://query.wikidata.org/sparql"
+    sparql = 'select * where{?entity rdfs:label ?label . filter (lang(?label) = "el") optional {?entity wdt:P19 ?birthPlace .} optional {?entity wdt:P569 ?birthDate .}    optional {?entity wdt:P21 ?gender .} optional {?entity wdt:P2278 ?mpID .} bind(exists {?entity wdt:P39 wd:Q18915989} as ?mpStatus) values ?entity { wd:' + id +'}  }'
+    client = SPARQL::Client.new(endpoint, :method => :get)
+    rows = client.query(sparql)
+    original["hellenic_parliament_id"] = mpId(rows[0])
+    original["birth_date"] = birthDate(rows[0])
+    original["birth_place"] = birthPlace(rows[0])
+    original["gender"] = gender(rows[0])
+    original["mpStatus"] = mpStatus(rows[0])
+    p "Found and updated"
+    original
+  rescue
+    p "Entity does not exist in wikidata"
+    original
+  end
+end
+
 # # Read in a page
-# page = agent.get("http://foo.com")
+api_key = ENV["MORPH_API_KEY"]
+url = "https://api.morph.io/everypolitician-scrapers/hellenic-parliament-wikipedia/data.json?key=" + api_key + "&query=select%20*%20from%20%22data%22"
+response = RestClient.get(url)
+json = JSON.parse(response)
 #
+json.each_with_index do |item,key|
+    p "-------------------------------------------------------------------------"
+    p item["name"]
+    json[key] = getWikidata(item["id"], item)
+    p "-------------------------------------------------------------------------"
+
+end
+
+
 # # Find somehing on the page using css selectors
-# p page.at('div.content')
+
 #
 # # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
+ScraperWiki.save_sqlite(["name"], json)
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
